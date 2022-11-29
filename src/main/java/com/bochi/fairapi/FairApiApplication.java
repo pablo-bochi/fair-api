81a02223c80f51c062bdad8b4557cf9b29e9be3e
Pablo Vilela <pablo.vilela@agronow.com.br>
2022-11-28 22:44:02 -0300

    created spring gradle project

---
+++ src/main/java/com/bochi/fairapi/FairApiApplication.java
@@ -0,0 +1,13 @@
+package com.bochi.fairapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bochi/fairapi/FairApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bochi.fairapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
